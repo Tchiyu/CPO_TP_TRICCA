3bc0556096553c24688bdfc34ad13ffafbadbc94
anton <anton@10.3.62.15>
2024-10-23 15:29:29 +0200

    Creation dun nouveau projet visionne

---
+++ TP3_Heroic_Fantasy_TRICCA/src/tp3_heroic_fantasy_tricca/TP3_Heroic_Fantasy_TRICCA.java
@@ -0,0 +1,36 @@
+package tp3_heroic_fantasy_tricca;
+
+
+import java.util.ArrayList;
+/**
+ *
+ * @author anton
+ */
+public class TP3_Heroic_Fantasy_TRICCA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Epee epee1 = new Epee("Excalibur", 7, 5);
+    Epee epee2 = new Epee("Durandal", 4, 7);
+    Baton baton1 = new Baton("Chêne", 4, 5 );
+    Baton baton2 = new Baton( "Charme", 5, 6);
+    
+     ArrayList<Arme> armes = new ArrayList<>();
+        armes.add(epee1);
+        armes.add(epee2);
+        armes.add(baton1);
+        armes.add(baton2);
+    for (int i = 0; i < armes.size(); i++) {
+            Arme arme = armes.get(i); // Utilisation de get() pour accéder à l'élément
+            arme.afficherInfo();
+            if (arme instanceof Epee) {
+                ((Epee) arme).trancher();
+            } else if (arme instanceof Baton) {
+                ((Baton) arme).frapper();
+            }
+    }
+    
+}
